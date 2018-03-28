04aac657b635720d08697954830c2496d0201332
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-28 15:56:34 -0500

    adding colorServlet changes

--- src/main/java/ColorServlet.java
+++ src/main/java/ColorServlet.java
@@ -1,10 +1,14 @@
-@javax.servlet.annotation.WebServlet(name = "ColorServlet")
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "ColorServlet", urlPatterns = "/color")
 public class ColorServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+        String usercolor = request.getParameter("mycolor");
+        request.setAttribute("mycolor", usercolor);
+        request.getRequestDispatcher("color_redirect.jsp").forward(request, response);
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+        request.getRequestDispatcher("color.jsp").forward(request, response);
     }
 }
