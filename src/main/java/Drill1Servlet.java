2a4ed93621f2d68b2eaf0db742a78304cc07787e
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-28 15:57:30 -0500

    adding drillServevlet updates

--- src/main/java/Drill1Servlet.java
+++ src/main/java/Drill1Servlet.java
@@ -1,10 +1,14 @@
-@javax.servlet.annotation.WebServlet(name = "Drill1Servlet")
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "Drill1Servlet", urlPatterns = "/name")
 public class Drill1Servlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+        String name = request.getParameter("username");
+        request.setAttribute("username", name);
+        request.getRequestDispatcher("drill1_redirect.jsp").forward(request, response);
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+        request.getRequestDispatcher("drill1.jsp").forward(request, response);
     }
 }
