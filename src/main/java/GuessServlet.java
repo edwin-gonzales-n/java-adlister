84b3b389dec6fa98e7b0d2c7b1b7d1f29425af90
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-28 15:58:32 -0500

    adding guessServlet updates before switching branches

--- src/main/java/GuessServlet.java
+++ src/main/java/GuessServlet.java
@@ -1,10 +1,35 @@
-@javax.servlet.annotation.WebServlet(name = "GuessServlet")
-public class GuessServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
 
+@WebServlet(name = "GuessServlet",urlPatterns = "/guess")
+public class GuessServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Random rand = new Random();
+        int  n = rand.nextInt(100) + 1;
+        int userGuesses = 0;
+
+        int userNumber= Integer.parseInt(request.getParameter("mynumber"));
+        String youWon = "YES!  YOU WON!";
+        String tryAgain = "<a href=\"/guess\">Click to try again</a>";
+
+        if (userNumber == n){
+            request.setAttribute("won", youWon);
+        } else
+            request.setAttribute("lost",tryAgain);
+
+        request.setAttribute("mynumber", userNumber);
+        request.setAttribute("random",n);
+        request.getRequestDispatcher("guess_redirect.jsp").forward(request, response);
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("guess.jsp").forward(request, response);
     }
 }
